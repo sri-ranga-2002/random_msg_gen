4089501c230df41c94adbe5a41982aabd552970b
sri-ranga-2002 <106913504+sri-ranga-2002@users.noreply.github.com>
2024-03-20 10:57:00 +0000

    due to errors inline java script is added to the html page itself

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-document.getElementById("sendButton").addEventListener("click", function() {
-    // Send an HTTP request to the server to retrieve the random message
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://localhost:5000/get_random_message", true);
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        // Display the random message in the alert dialog
-        alert(xhr.responseText);
-      }
-    };
-    xhr.send();
+document.addEventListener("DOMContentLoaded", function() {
+    // Select the Send button element
+    const sendButton = document.getElementById("sendButton");
+  
+    // Add an event listener to the Send button
+    sendButton.addEventListener("click", function() {
+      // Make an HTTP GET request to the server to get a random message
+      fetch("/get_random_message")
+        .then(response => response.text())
+        .then(message => {
+          // Display the random message
+          alert(message);
+        })
+        .catch(error => {
+          console.error("Error fetching random message:", error);
+        });
+    });
   });
   
